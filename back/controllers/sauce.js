e936edf41ead7c5582b4dc9765bc2868d8868daa
Marion L <mlelaumier@gmail.com>
2022-05-16 13:44:29 +0100

    changed the name of the file, updated the newSauce object, renamed all product to sauce and deleted the {}

---
+++ back/controllers/sauce.js
@@ -0,0 +1,40 @@
+const Sauce = require("../models/sauce");
+const fs = require("fs");
+
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce Enregistrée !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(404).json({ error }));
+};
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+exports.modifySauce = (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then((sauce) => res.status(201).json(sauce))
+    .catch((error) => res.status(400).json({ error }));
+};
+exports.deleteSauce = (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(400).json({ error }));
+};
