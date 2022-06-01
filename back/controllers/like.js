7f0dfce4ee461f52b766ecd1d468f4e4f76a8219
Marion L <mlelaumier@gmail.com>
2022-06-01 10:04:28 +0100

    finished code managing likes

--- back/controllers/like.js
+++ back/controllers/like.js
@@ -1,30 +1,66 @@
+const sauce = require("../models/sauce");
 const Sauce = require("../models/sauce");
 
 exports.like = (req, res, next) => {
-  // case 1: user likes sauce like=1 + push in array
-  // => get userId from request and push it in array
-
-  if (req.body.like === 1) {
-    Sauce.findOneAndUpdate(
-      { _id: req.params.id },
-      {
-        $inc: { likes: req.body.like },
-        $addToSet: { usersLiked: req.body.userId },
+  console.log(req.body);
+  let userId = req.body.userId;
+  //find sauce to update
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      // case 1: user likes sauce like=1
+      // => get userId from request and push it in usersLiked array and pull it from usersDisliked array
+      if (req.body.like == 1 && !sauce.usersLiked.includes(userId)) {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: { likes: req.body.like },
+            $addToSet: { usersLiked: req.body.userId },
+            $pull: { usersDisliked: req.body.userId },
+          }
+        )
+          .then((sauce) => res.status(201).json(sauce))
+          .catch((error) => res.status(400).json({ error }));
       }
-    )
-      .then((sauce) => res.status(201).json(sauce))
-      .catch((error) => res.status(400).json({ error }));
-  }
-  //chercher le userId dans le tableau de like et si deja la alors mettre like=0 et retirer du tableau
+      //cas 2: user dislikes sauce like = -1
+      // => get userId from request and push it in usersDisliked array and pull it from usersLiked array
+      if (req.body.like == -1 && !sauce.usersDisliked.includes(userId)) {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: { dislikes: req.body.like },
+            $addToSet: { usersDisliked: req.body.userId },
+            $pull: { usersLiked: req.body.userId },
+          }
+        )
 
-  //cas 2: user dislikes sauce like = -1 + push in array
-  // => get userId from request qnd push it in array
-  if (req.body.like === -1) {
-    console.log("product disliked");
-  }
-  // cas 3: user retire son like ou son dislike like=0 + delete from array
-  //=> find indexOF userId in arrays and splice it
-  if (req.body.like === 0) {
-    console.log("product not liked or disliked");
-  }
+          .then((sauce) => res.status(201).json(sauce))
+          .catch((error) => res.status(400).json({ error }));
+      }
+      // case 3: user takes off his like or dislike => like=0 + delete from all arrays
+      //case 3a: user takes off his like
+      if (req.body.like == 0 && sauce.usersLiked.includes(userId)) {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: { likes: -1 },
+            $pull: { usersLiked: req.body.userId },
+          }
+        )
+          .then((sauce) => res.status(201).json(sauce))
+          .catch((error) => res.status(400).json({ error }));
+      }
+      //case 3b: user takes off his dislike
+      else if (req.body.like == 0 && sauce.usersDisliked.includes(userId)) {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: { dislikes: 1 },
+            $pull: { usersDisliked: req.body.userId },
+          }
+        )
+          .then((sauce) => res.status(201).json(sauce))
+          .catch((error) => res.status(400).json({ error }));
+      }
+    })
+    .catch((error) => res.status(400).json({ error }));
 };
