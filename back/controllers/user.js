6feb991c7b2ddd4bfa8e6e6a8c6ddc85da6f1227
Marion L <mlelaumier@gmail.com>
2022-06-07 10:23:51 +0100

    added checks on the password strength

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,21 +1,62 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+//function to check a strong password was typed.
+function checkPasswordValidation(value) {
+  const isWhitespace = /^(?=.*\s)/;
+  if (isWhitespace.test(value)) {
+    return "Le mot de passe ne peut contenir d'espaces.";
+  }
+
+  const isContainsUppercase = /^(?=.*[A-Z])/;
+  if (!isContainsUppercase.test(value)) {
+    return "Le mot de passe doit contenir au minimum 1 majuscule.";
+  }
+
+  const isContainsLowercase = /^(?=.*[a-z])/;
+  if (!isContainsLowercase.test(value)) {
+    return "Le mot de passe doit contenir au minimum 1 minuscule.";
+  }
+
+  const isContainsNumber = /^(?=.*[0-9])/;
+  if (!isContainsNumber.test(value)) {
+    return "Le mot de passe doit contenir au minimum 1 chiffre.";
+  }
+
+  const isContainsSymbol = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
+  if (!isContainsSymbol.test(value)) {
+    return "Le mot de passe doit contenir au minimum 1 symbole.";
+  }
+
+  const isValidLength = /^.{10,16}$/;
+  if (!isValidLength.test(value)) {
+    return "Le mot de passe doit contenir entre 10 et 16 caratères.";
+  }
+  return null;
+}
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  //checking if the password is strong, if so password goes through Bcrypt
+  if (checkPasswordValidation(req.body.password) == null) {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash,
+        });
+        user
+          .save()
+          .then(() => res.status(201).json({ message: "Utilisateur créé" }))
+          .catch((error) => res.status(400).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  } else {
+    return res.status(401).json({
+      message:
+        "Le mot de passe doit contenir entre 10 et 16 caratères, dont 1 majuscule, 1 chiffre, 1 symbole et sans espaces",
+    });
+  }
 };
 
 exports.login = (req, res, next) => {
