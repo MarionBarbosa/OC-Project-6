a7e3b338127995b84bf17b515d829543e5a46380
Marion L <mlelaumier@gmail.com>
2022-06-07 10:26:27 +0100

    updated regex for email

--- back/models/user.js
+++ back/models/user.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 const validateEmail = (email) => {
-  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailRegex.test(email)) {
+    return false;
+  }
+  return true;
 };
+
 const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true,
     validate: validateEmail,
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
   },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("User", userSchema);
